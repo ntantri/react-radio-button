49be661c879637cb94902e3f93dd8ba3a8f732eb
tan31989 <nagarajtantri@gmail.com>
2016-07-03 09:01:11 +0530

    Added the initial code for component and added the css

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+
+import '~/styles/radiobutton.less';
+
+
+class RadioButtonGroup extends Component {
+
+  constructor(props) {
+    super(props);
+    this.listOfItems = props.listOfItems;
+    this.selectedItemCallback = props.selectedItemCallback;
+    this.state = {optionsVal: ''};
+  }
+
+  handleQueryOpChange(e) {
+    this.setState({
+      optionsVal: e.target.value
+    });
+    this.selectedItemCallback(e.target.value);
+  }
+
+  render() {
+    const renderItems = (item) => {
+      return (
+          <label htmlFor={item.value} key={item.value}
+            className={'btn btn-default radio-button-item ' + (this.state.optionsVal === item.value ? 'checked' : '')}>
+              <input type="radio" name="options" value={item.value}
+                checked={this.state.optionsVal === item.value}
+                onChange={this.handleQueryOpChange.bind(this)} id={item.value} />
+              <span className="radio-dot"></span>
+              <span className="radio-button-item-word">{item.text}</span>
+          </label>
+        );
+    };
+
+    return (
+      <div data-toggle="buttons">
+        {this.listOfItems.map((item) => renderItems(item))}
+      </div>
+    );
+  }
+}
+
+
+RadioButtonGroup.propTypes = {
+  listOfItems: PropTypes.array,
+  selectedItemCallback: PropTypes.func.isRequired
+};
+
+
+export default connect()(RadioButtonGroup)
